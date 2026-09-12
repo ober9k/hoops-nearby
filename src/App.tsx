f4e3aa7ae34bcc519c35e5955983ee3fc3cbe5b2
Dean Hardaker <ober9k@users.noreply.github.com>
2026-09-12 18:28:23 +1000

    Add test result for collections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import './App.css';
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { firestoreDb } from "./firebase";
 
 function App() {
+  const [ courts, setCourts ] = useState<any[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(firestoreDb, "courts"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const courtsArray: any[] = [];
+      querySnapshot.forEach((doc) => {
+        courtsArray.push({ ...doc.data(), id: doc.id });
+      });
+      setCourts(courtsArray);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  });
+
   return (
     <>
       <section>
-        Hello World.
+        <h2>Local Courts</h2>
+        <p>Hello World.</p>
+        <ul>
+          {courts.map((court, key) => (
+            <li key={key}>
+              <h3>{court.location}</h3>
+              <p>{court.suburb}: {court.courts?.length || 0} court(s)</p>
+            </li>
+          ))}
+        </ul>
       </section>
     </>
   );
